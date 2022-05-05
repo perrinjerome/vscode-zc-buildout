cb8246ae1804e7197582bc4bdf9879cdd09bc172
Jérome Perrin <perrinjerome@gmail.com>
2022-05-05 23:02:56 +0900

    multi line options and comments

--- tree-sitter-zcbuildout/grammar.js
+++ tree-sitter-zcbuildout/grammar.js
@@ -4,18 +4,16 @@ module.exports = grammar({
   extras: ($) => [
     /[ \t]+\n*/, // Ignore empty lines.
   ],
-
+  conflicts: ($) => [
+    // [$.option_value]
+  ],
   rules: {
     profile: ($) =>
       seq(/[\n]*/, optional(repeat($.comment)), repeat(seq($.section))),
 
     section: ($) =>
       prec.left(
-        seq(
-          $._section_header,
-          /[\n]+/,
-          repeat(choice(seq($.option, /[\n]+/), $.comment))
-        )
+        seq($._section_header, /[\n]+/, repeat(choice($.option, $.comment)))
       ),
 
     _section_header: ($) =>
@@ -23,22 +21,35 @@ module.exports = grammar({
     section_name: ($) => /[^\[\]:\n]+/,
     section_condition: ($) => /[^\[\]\n]+/,
 
-    // TODO: multi line options
     option: ($) => seq($.option_name, "=", $.option_value),
 
-    option_name: ($) => /[^#=\s\[]+/,
+    option_name: ($) => /[^#;=\s\[]+/,
     option_value: ($) =>
+      choice(
+        seq(
+          $._option_value_mono_line,
+          prec.left(1, repeat($._option_value_multi_line))
+        ),
+        seq(/\n/, prec.left(2, repeat1($._option_value_multi_line)))
+      ),
+    _option_value_mono_line: ($) => prec.left(1, seq($.option_text, /\n+/)),
+    _option_value_multi_line: ($) =>
+      prec.left(2, repeat1(seq(/\s+/, $.option_text, /\n+/))),
+
+    option_text: ($) => /[^\n]+/,
+    xoption_value: ($) =>
       seq(
         optional($.option_text),
         repeat1(seq($.option_with_reference, optional($.option_text)))
       ),
 
+    // option = text maybe ${ok:ok}
     option_with_reference: ($) =>
       seq("${", optional($.referenced_section), ":", $.referenced_option, "}"),
     referenced_section: ($) => /[^:\}\n]+/,
     referenced_option: ($) => /[^\n\}]+/,
-    option_text: ($) => choice(/[^\$\n]+[^\n]*/, /[^\$\n]\$\{[^\}\n]+/),
+    xoption_text: ($) => choice(/[^\$\n]+[^\n]*/, /[^\$\n]\$\{[^\}\n]+/),
 
-    comment: ($) => token(prec(-10, /#.*\n+/)),
+    comment: ($) => token(prec(-10, /[#;]+.*\n+/)),
   },
 });
